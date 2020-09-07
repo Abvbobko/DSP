b27b90744684b3940f40ba63449aae53257b47c7
Alexei Bobko <abvbobko@gmail.com>
2020-09-08 01:19:12 +0300

    Complete task 2

--- data/data_storage.py
+++ data/data_storage.py
@@ -7,10 +7,10 @@ class DataStorage:
     NUM_OF_OPTIONS = 6
 
     def __init__(self, data_a, data_b, data_v, data_g):
-        self.table_a = DataStorage.create_df(data_a)
-        self.table_b = DataStorage.create_df(data_b)
-        self.table_v = DataStorage.create_df(data_v)
-        self.table_g = [DataStorage.generate_table_g(A=data_g['A'][i], phi=data_g['phi'][i]) for i in range(6)]
+        self._table_a = DataStorage.create_df(data_a)
+        self._table_b = DataStorage.create_df(data_b)
+        self._table_v = DataStorage.create_df(data_v)
+        self._table_g = [DataStorage.generate_table_g(A=data_g['A'][i], phi=data_g['phi'][i]) for i in range(6)]
 
     @staticmethod
     def create_df(data, num_of_options=NUM_OF_OPTIONS):
@@ -32,23 +32,23 @@ class DataStorage:
         return DataStorage.create_df(data=data, num_of_options=5)
 
     def get_a(self, option):
-        if option in self.table_a.index:
-            return self.table_a.loc[option]
+        if option in self._table_a.index:
+            return self._table_a.loc[option]
         return None
 
     def get_b(self, option):
-        if option in self.table_b.index:
-            return self.table_b.loc[option]
+        if option in self._table_b.index:
+            return self._table_b.loc[option]
         return None
 
     def get_v(self, option):
-        if option in self.table_v.index:
-            return self.table_v.loc[option]
+        if option in self._table_v.index:
+            return self._table_v.loc[option]
         return None
 
     def get_g(self, option):
-        if (option-1) < len(self.table_g):
-            return self.table_g[option - 1]
+        if (option-1) < len(self._table_g):
+            return self._table_g[option - 1]
         return None
 
 
--- main.py
+++ main.py
@@ -1,4 +1,55 @@
+import matplotlib.pyplot as plt
+from data import data_storage
+from signals import harmonic
+
 
 if __name__ == '__main__':
-    option = int(input())
-        
+    option = 1  # int(input())
+    storage = data_storage.generate_data()
+
+    # task 2a
+    table_a = storage.get_a(option)
+
+    fig, ax = plt.subplots(5, 3)
+    A = table_a.A
+    f = table_a.f
+    N = 512
+    for i in range(1, 6):
+        n_list = list(range(N))
+
+        x = [
+            harmonic.harmonic_signal(A=A, f=f, n=n, N=N, phi=table_a[f'phi{i}'])
+            for n in n_list
+        ]
+
+        ax[i - 1][0].plot(n_list, x)
+
+    # task 2b
+    table_b = storage.get_b(option)
+    A = table_b.A
+    phi = table_b.phi
+    for i in range(1, 6):
+        n_list = list(range(N))
+
+        x = [
+            harmonic.harmonic_signal(A=A, f=table_b[f'f{i}'], n=n, N=N, phi=phi)
+            for n in n_list
+        ]
+
+        ax[i - 1][1].plot(n_list, x)
+
+    # task 2v
+    table_v = storage.get_v(option)
+    f = table_v.f
+    phi = table_v.phi
+    for i in range(1, 6):
+        n_list = list(range(N))
+
+        x = [
+            harmonic.harmonic_signal(A=table_v[f'A{i}'], f=f, n=n, N=N, phi=phi)
+            for n in n_list
+        ]
+
+        ax[i - 1][2].plot(n_list, x)
+
+    plt.show()
