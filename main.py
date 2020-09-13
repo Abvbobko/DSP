ba4806ca706a27a0b2d86cbd9279c6860cf7ba30
Alexei Bobko <abvbobko@gmail.com>
2020-09-13 23:06:37 +0300

    Optimize code in task 3

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ import task4_tmp
 
 if __name__ == '__main__':
     option = 1  # int(input())
+    N = 512  # int(input())
     storage = data_storage.generate_data()
-    task2_tmp.task2(option=option, storage=storage, N=512)
-    # task3_tmp.task3(option=option, storage=storage, N=512)
-    # task4_tmp.task4(option, storage, N=512, num_of_periods=4)
+    # task2_tmp.task2(option=option, storage=storage, N=N)
+    task3_tmp.task3(option=option, storage=storage, N=N)
+    # task4_tmp.task4(option, storage, N=N, num_of_periods=4)
 
--- task3_tmp.py
+++ task3_tmp.py
@@ -4,41 +4,50 @@ from signals import polyharmonic
 import pandas as pd
 
 
-def task3(storage, option, N):
-    table_g = storage.get_g(option)
-
-    # N = 512
+def create_signal(k, N, A, f, phi, ax, plot_column):
     n_list = list(range(N))
+    # for i in range(len(A)):
+    x = [
+        polyharmonic.polyharmonic_signal(k=k, A=A, N=N, f=f, n=n, phi=phi)
+        for n in n_list
+    ]
+    ax[plot_column].plot(n_list, x)
+
+
+def task3(storage, option, N):
+    SIZE = 5
+    table_g = storage.get_g(option)
 
     # task 3.1 (просто таблица)
     fig, ax = plt.subplots()
-    x = [
-        polyharmonic.polyharmonic_signal(k=5, A=table_g.A, N=N, f=table_g.f, n=n, phi=table_g.phi)
-        for n in n_list
-    ]
-    ax.plot(n_list, x)
+    create_signal(k=5, A=table_g.A, N=N, f=table_g.f, phi=table_g.phi, ax=[ax], plot_column=0)
+
     plt.show()
 
     # task 3.2 (изменять phi)
-    fig, ax = plt.subplots(5, 2)
+    fig, ax = plt.subplots(SIZE, 2)
+
     c_offset = pi/4
-    d_offset = pd.Series([0, pi/2, pi/3, pi/4, pi], index=[i for i in range(1, 6)])
+    d_offset = pd.Series([0, pi/2, pi/3, pi/4, pi], index=[i for i in range(1, SIZE+1)])
     phi_const_offset = table_g.phi.copy()
     phi_diff_offset = table_g.phi.copy()
-    for i in range(1, 6):
-        x1 = [
-            polyharmonic.polyharmonic_signal(k=5, A=table_g.A, N=N, f=table_g.f, n=n, phi=phi_const_offset)
-            for n in n_list
-        ]
 
-        x2 = [
-            polyharmonic.polyharmonic_signal(k=5, A=table_g.A, N=N, f=table_g.f, n=n, phi=phi_diff_offset)
-            for n in n_list
-        ]
-
+    for i in range(1, SIZE+1):
+        create_signal(
+            k=5, N=N, ax=ax[i-1],
+            A=table_g.A,
+            f=table_g.f,
+            phi=phi_const_offset,
+            plot_column=0
+        )
+        create_signal(
+            k=5, N=N, ax=ax[i-1],
+            A=table_g.A,
+            f=table_g.f,
+            phi=phi_diff_offset,
+            plot_column=1
+        )
         phi_const_offset += c_offset
         phi_diff_offset += d_offset
-        ax[i - 1][0].plot(n_list, x1)
-        ax[i - 1][1].plot(n_list, x2)
-    print(phi_diff_offset)
+
     plt.show()
